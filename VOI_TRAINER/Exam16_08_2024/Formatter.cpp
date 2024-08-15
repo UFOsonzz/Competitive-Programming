245f6c99291f0a03d6c340d3892160ca4afe653e
UFOsonzz <tamzuka2006@gmail.com>
2024-08-15 15:52:30 +0700

    Reformatted Exam16_08_2024 for Themis

---
+++ VOI_TRAINER/Exam16_08_2024/Formatter.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int ProbNum = 3;
+const int TestNum = 21;
+
+int main() {
+    
+    for (int i = 1; i <= TestNum; i++) {
+        string folder_name = "test" + to_string(i);
+
+        string inp_name = "test" + to_string(i) + ".inp";
+        string newinp_name = "P" + to_string(ProbNum) + ".inp";
+
+        string out_name = "test" + to_string(i) + ".out";
+        string newout_name = "P" + to_string(ProbNum) + ".out";
+
+        if ((system(("rename " + inp_name + " " + newinp_name).c_str()) == 0) &&
+            (system(("rename " + out_name + " " + newout_name).c_str()) == 0) &&
+            (system(("mkdir " + folder_name).c_str()) == 0) &&
+            (system(("move " + newinp_name + " " + folder_name).c_str()) == 0) &&
+            (system(("move " + newout_name + " " + folder_name).c_str()) == 0)) {
+            cerr << "Created folder " << folder_name << "!\n"; 
+        } else {
+            cerr << "Error when creating folder " << folder_name << "!!!\n";
+        }
+    }
+}

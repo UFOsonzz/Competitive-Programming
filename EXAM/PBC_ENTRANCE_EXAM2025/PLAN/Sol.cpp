90fe5ca49427046653d0d56d0fff05cffa7e6e90
UFOsonzz <tamzuka2006@gmail.com>
2025-06-10 23:25:46 +0700

    Modified a small error in PBC_ENTRANCE_EXAM2025

--- EXAM/PBC_ENTRANCE_EXAM2025/PLAN/Sol.cpp
+++ EXAM/PBC_ENTRANCE_EXAM2025/PLAN/Sol.cpp
@@ -11,7 +11,7 @@ int main() {
     int ans = 1;
     for (char c = 'a'; c <= 'z'; c++) {
         int l = 0, cur_cost = 0;
-        for (int i = 1; i < s.size(); i++) {
+        for (int i = 0; i < s.size(); i++) {
             cur_cost += abs(s[i] - c);
             while (cur_cost > k) {
                 cur_cost -= abs(s[l++] - c);
